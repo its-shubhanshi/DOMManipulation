253f7960f79de99b768c47144bc74073936d1ae2
its-shubhanshi <itsshubhanshi23@gmail.com>
2025-04-04 00:21:14 +0530

    secondproject

---
+++ InstaLikeButton/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let con = document.querySelector("#container");
+let like = document.querySelector("i");
+
+con.addEventListener("dblclick", function () {
+  like.style.transform = " translate(-50%, -50%) scale(1)";
+  like.style.color = "red";
+  like.style.opacity = 0.8;
+
+  setTimeout(function () {
+    
+    like.style.transform = " translate(-50%, -50%) scale(0)";
+    }, 1000);
+    
+  setTimeout(function () {
+    
+  like.style.transform = " translate(-50%, -50%) scale(0)";
+  }, 2000);
+});
